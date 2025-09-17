439fb64aa260116fc2e3bdb7035328ecfeaee1df
vanshdubey <vanshdubey101963@gmail.com>
2025-09-17 10:24:34 +0530

    relative imports fix

---
+++ main.py
@@ -0,0 +1,7 @@
+def main():
+    pass
+
+from src.agents.supervisor_agent.supervisor import create_supervisor
+
+if __name__ == "__main__":
+   create_supervisor(None)
--- src/main.py
+++
@@ -1,8 +0,0 @@
-def main():
-    pass
-
-from agents.coding_agent.memory import DB_PATH , ProjectState, checkpoint
-
-if __name__ == "__main__":
-    print(DB_PATH)
-    print(checkpoint)
